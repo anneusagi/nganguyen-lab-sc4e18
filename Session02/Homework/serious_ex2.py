c0917012f75a31e70ec5c9d1d050b0adbc3f06d5
anneusagi <ntnga165@gmail.com>
2018-06-14 09:12:02 +0700

    hichic

---
+++ Session02/Homework/serious_ex2.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen,urlretrieve
+from bs4 import BeautifulSoup
+from pyexcel import *
+
+url = "http://s.cafef.vn/bao-cao-tai-chinh/VNM/IncSta/2017/3/0/0/ket-qua-hoat-dong-kinh-doanh-cong-ty-co-phan-sua-viet-nam.chn"
+html_content = urlopen(url).read().decode("utf-8")
+
+
+soup = BeautifulSoup(html_content, "html.parser")
+table = soup.find("table",id = "tableContent")
+
