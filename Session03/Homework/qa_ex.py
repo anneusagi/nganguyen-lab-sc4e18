09d9d3c4d12ba0639233bfbb504956b146302267
anneusagi <ntnga165@gmail.com>
2018-06-17 11:36:42 +0700

    meow

---
+++ Session03/Homework/qa_ex.py
@@ -0,0 +1,12 @@
+Why should we use functions at all?
+- Để tiết kiệm thời gian, công code lại. 
+How to define/declare a function?
+- def functionname( parameters ):
+   "function_docstring"
+   function_suite
+   return [expression]
+How to call/use a function?
+What is return, why and how do we use it?
+Do we have to use return in every function?
+What are function arguments/parameters, why and how we use it?
+How to use function from a different file other than our currently working file?
